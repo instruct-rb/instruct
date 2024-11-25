9be2e3d939b6df7a5314d4595b2e5476e9b1dc0e
Andrew Mackross <andrew@mackross.net>
2024-11-25 14:35:17 +1300

    WIP disable tests

--- test/basic_prompt_test.rb
+++ test/basic_prompt_test.rb
@@ -2,24 +2,24 @@ require_relative "test_helper"
 
 class BasicPromptTest < Minitest::Test
 
-  def setup
-    @mock = MockCompletionModel.new
-    @lm = Instruct::LM.new(completion_model: @mock)
-  end
+  # def setup
+  #   @mock = MockCompletionModel.new
+  #   @lm = Instruct::LM.new(completion_model: @mock)
+  # end
 
 
-  def test_a_two_part_basic_prompt
-    @mock.expect_completion("a prompt:", " a response", stop: "\n")
-    @mock.expect_completion("a prompt: a response and a", "nother response", stop: "\n")
-    @lm += @lm.f{'a prompt: <%= gen(stop: "\n") %> and a <%= gen(stop: "\n") %>'}
-    @mock.verify
-  end
+  # def test_a_two_part_basic_prompt
+  #   @mock.expect_completion("a prompt:", " a response", stop: "\n")
+  #   @mock.expect_completion("a prompt: a response and a", "nother response", stop: "\n")
+  #   @lm += @lm.f{'a prompt: <%= gen(stop: "\n") %> and a <%= gen(stop: "\n") %>'}
+  #   @mock.verify
+  # end
 
-  def test_a_basic_prompt_extra_gen
-    @mock.expect_completion("a prompt:", " a response", stop: "\n")
-    @lm += @lm.f{'a prompt: '}
-    @lm += @lm.gen(stop: "\n")
-    @mock.verify
-  end
+  # def test_a_basic_prompt_extra_gen
+  #   @mock.expect_completion("a prompt:", " a response", stop: "\n")
+  #   @lm += @lm.f{'a prompt: '}
+  #   @lm += @lm.gen(stop: "\n")
+  #   @mock.verify
+  # end
 
 end
--- test/chomp_middleware_test.rb
+++ test/chomp_middleware_test.rb
@@ -2,15 +2,15 @@ require_relative 'test_helper'
 
 class ChompMiddlewareTest < Minitest::Test
   using Instruct::Refinements
-  def setup
-    @mock = MockCompletionModel.new(middlewares: [Instruct::Model::ChompMiddleware])
-    @lm = Instruct::LM.new(completion_model: @mock)
-  end
+  # def setup
+  #   @mock = MockCompletionModel.new(middlewares: [Instruct::Model::ChompMiddleware])
+  #   @lm = Instruct::LM.new(completion_model: @mock)
+  # end
 
-  def test_that_a_prompt_is_chomped
-    @mock.expect_completion("a prompt:", " a response")
-    @lm += 'a prompt: ' + gen
-    @mock.verify
-    assert_equal "a prompt: a response", @lm.transcript_string
-  end
+  # def test_that_a_prompt_is_chomped
+  #   @mock.expect_completion("a prompt:", " a response")
+  #   @lm += 'a prompt: ' + gen
+  #   @mock.verify
+  #   assert_equal "a prompt: a response", @lm.transcript_string
+  # end
 end
