eedcfb595694250a5e90cea3c2d774c1de48a10d
Andrew Mackross <andrew@mackross.net>
2024-12-04 00:30:29 +1300

    Add a multi-agent streaming test
    This test should just run, there is no testing the output

---
+++ test/openai/multi_agent_test.rb
@@ -0,0 +1,52 @@
+
+require "test_helper"
+require "ruby/openai"
+
+# These tests could be flakey as they are based on llm responses
+class OpenAIBasicTest < Minitest::Test
+  include Instruct::Helpers
+  using Instruct::Refinements
+
+  def setup
+    self._instruct_default_model = 'gpt-4o-mini'
+  end
+
+  def test_it_works
+    # Create two agents: Noel Gallagher and an interviewer with a system prompt.
+    noel = p{"system: You're Noel Gallagher. Answer questions from an interviewer. Don't end the conversation."}
+    interviewer = p{"system: You're a skilled interviewer asking Noel Gallagher questions. Don't end the conversation find new topics."}
+
+    # We start a dynamic Q&A loop with the interviewer by kicking off the
+    # interviewing agent and capturing the response under the :reply key.
+    interviewer << p{"\nuser: __Noel sits down in front of you.__"} + gen.capture(:reply)
+
+    puts interviewer.captured(:reply) # => "Hello Noel, how are you today?"
+
+    5.times do
+      # Noel is sent the last value captured in the interviewer's transcript under the :reply key.
+      # Similarly, we generate a response for Noel and capture it under the :reply key.
+      print "Noel: "
+      noel << p{"\nuser: <%= interviewer.captured(:reply) %>"}
+      prompt = noel + gen.capture(:reply, list: :replies)
+      resp = prompt.call do |resp|
+        print resp.get_chunk
+      end
+      print "\n\n"
+      noel = prompt + resp
+
+
+      # Noel's captured reply is now sent to the interviewer, who captures it in the same way.
+      interviewer << p{"\nuser: <%=  noel.captured(:reply) %>"} + gen.capture(:reply, list: :replies)
+      print "Interviewer: "
+      print interviewer.captured(:reply)
+      print "\n\n"
+    end
+
+    # After the conversation, we can access the list captured replies from both agents
+    noel_said = noel.captured(:replies).map{ |r| "noel: #{r}" }
+    interviewer_said = interviewer.captured(:replies).map{ |r| "interviewer: #{r}" }
+
+    puts interviwer_said.zip(noel_said).flatten.join("\n\n")
+    # => "noel: ... \n\n interviewer: ..., ..."
+  end
+end
